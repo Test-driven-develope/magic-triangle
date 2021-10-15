84883c60827709f2e941347a1a4adc51b1fa2ce6
Jinghua Li <jinghuali@melco-resorts.com>
2021-10-15 17:34:52 +0800

    init project

---
+++ src/main/java/com/eason/magictriangle/TriangleApp.java
@@ -0,0 +1,8 @@
+package com.eason.magictriangle;
+
+public class TriangleApp {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用魔幻三角小程序！");
+        System.out.println("请您需要判断的三角形的三条边长，如2,2,4:");
+    }
+}
