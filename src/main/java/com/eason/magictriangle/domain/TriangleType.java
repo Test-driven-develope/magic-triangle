e5a05f0352054a7f5e2c34c07370ff7b1452cba1
Jinghua Li <jinhli@thoughtworks.com>
2022-05-16 18:27:00 +0800

    Use enum method to seperate logic

--- src/main/java/com/eason/magictriangle/domain/TriangleType.java
+++ src/main/java/com/eason/magictriangle/domain/TriangleType.java
@@ -1,11 +1,34 @@
 package com.eason.magictriangle.domain;
 
 public enum TriangleType {
-    NORMAL("常规三角形"), EQUILATERAL("等边三角形"), ISOSCELES("等腰三角形"), RIGHT("直角三角形");
-    
+    EQUILATERAL("等边三角形") {
+        @Override
+        public boolean isCurrentType(Triangle triangle) {
+            return triangle.firstSide == triangle.secondSide
+                    && triangle.secondSide == triangle.thirdSide;
+        }
+    },
+    ISOSCELES("等腰三角形") {
+        @Override
+        public boolean isCurrentType(Triangle triangle) {
+            return triangle.firstSide == triangle.secondSide;
+        }
+    },
+    RIGHT("直角三角形") {
+        @Override
+        public boolean isCurrentType(Triangle triangle) {
+            return (Math.pow(triangle.firstSide, 2) + Math.pow(triangle.secondSide, 2))
+                    == Math.pow(triangle.thirdSide, 2);
+        }
+    }, NORMAL("常规三角形");
+
     public final String typeName;
-    
+
     TriangleType(String typeName) {
         this.typeName = typeName;
+    }
+
+    public boolean isCurrentType(Triangle triangle) {
+        return triangle.firstSide + triangle.secondSide > triangle.thirdSide;
     }
 }
